132463d92adeda0055ed888e586ffd7a8425d8ce
WEBHM2-MAIN1-MAIN 2 <taharami14@gmail.com>
2025-08-03 15:52:31 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
 import Home from "./components/home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
   {
     path: "*",
-    element: <App />,
-  },
-  {
-    path: "/app",
-    element: <App />,
+    element: <Home />,
   },
   {
     path: "/home",
